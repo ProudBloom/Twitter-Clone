09fbf26f9fd6db81d6c64029c792ef5c3a5a168c
ProudBloom <jakub-sztompka@wp.pl>
2020-08-17 14:08:41 +0200

    Added GET request handler to the server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ const express = require('express');     //Add a library using node
 
 const app = express();
 
+app.get('/', (request, response) =>
+{
+    response.json(
+        {
+            messgae: 'Hello!',
+        }
+    )
+});
+
 app.listen(5000, () =>
 {
     console.log('Listening on 5000')
