80d1509186def1d0863b8e9ad37a859541672ec0
ProudBloom <jakub-sztompka@wp.pl>
2020-08-19 15:41:59 +0200

    Input validation and Injecting the data to local DB using monk [JS - server]

--- client/client.js
+++ client/client.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const loadingGif = document.querySelector('.loading');
-const API_URL = 'http://localhost:5000/woofs';
+const API_URL = 'http://localhost:8000/woofs';
 
 loadingGif.style.display = 'none';
 
@@ -12,19 +12,17 @@ form.addEventListener('submit', (event) =>
     const formData = new FormData(form);
     const userName = formData.get('name');
     const message = formData.get('content');
-
     const woof = {
         userName,
         message,
     };
+
     form.style.display = 'none'
     loadingGif.style.display = 'block';
 
     fetch(API_URL, {
         method: 'POST',
         body: JSON.stringify(woof),
-        headers: {
-            'content-type': 'application/json'
-        }
+        headers: {'content-type': 'application/json'}
     })
 });
--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');     //Add a library using node
 const cors = require('cors');
+const monk = require('monk');
 
 const app = express();                  //Add cors as a middleware to handle the headers
+const db = monk('localhost/woofer');
+const woofs = db.get('woofs');
+
 
 app.use(cors());
 app.use(express.json());                //Parses JSON body of the POST request
+
+function isWoofValid(woof)
+{
+    return woof.userName && woof.userName.toString().trim() !== '' && woof.message && woof.message.toString().trim() !== '';
+}
 
 app.get('/', (request, response) =>
 {
@@ -17,10 +26,30 @@ app.get('/', (request, response) =>
 
 app.post('/woofs', (request, response) => 
 {
-    console.log(request.body);
+    if(isWoofValid(request.body))
+    {
+        //Add to DB
+        const woof = {
+            userName: request.body.userName.toString(),
+            message: request.body.message.toString(),
+            created: new Date(),
+        }
+
+        woofs.insert().then(createdWoof => {
+            response.json(createdWoof);
+        })
+    }
+    else
+    {
+        response.status(422);
+        response.json({
+            messgae: 'Name and content are required.'
+        });
+    }
+    
 });
 
-app.listen(5000, () =>
+app.listen(8000, () =>
 {
-    console.log('Listening on 5000')
+    console.log('Listening on http://localhost:8000')
 });
